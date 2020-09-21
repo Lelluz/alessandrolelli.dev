6ba3f67ff55a4c2a8a1e2aed6b5e4c53ba966432
lellia <alessandro.lelli.external@ynap.com>
2020-09-21 12:33:15 +0200

    phpmailer init

--- sendmessage.php
+++ sendmessage.php
@@ -1,15 +1,34 @@
 <?php
 
+$host_mail = 'alessandro@alessandrolelli.dev';
 $mail_from = $_POST['mail'];
 $message = $_POST['message'];
+$body = 'New message from your website!<br><br>'
+      .'E-mail: '.$mail_from.'<br>'
+      .'Messaggio: '.$message.'<br><br>';
 
-$mail_to = "alelelli.98@gmail.com";
-$headers = "From: ".$mail_from;
-$subject = "Nuovo messaggio per te!";
-$txt = "Nuovo messaggio per te!\n\n\n"
-      ."E-mail: ".$mail_from."\n\n"
-      ."Messaggio: ".$message."\n\n";
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
 
-mail($mail_to, $subject, $txt, $headers);
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->IsSMTP();
 
-echo "OK";
+$mail->SMTPDebug = 0;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'server220.web-hosting.com';
+$mail->Port = 465;
+$mail->IsHTML(true);
+$mail->Username = $host_mail;
+$mail->Password = 'Anonymo07';
+$mail->SetFrom('alessandro@alessandrolelli.dev', 'Personal Website');
+$mail->Subject = 'New message from your website!';
+$mail->Body = $body;
+$mail->AddAddress('alelelli.98@gmail.com', 'Alessandro');
+
+if(!$mail->Send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'OK';
+}
