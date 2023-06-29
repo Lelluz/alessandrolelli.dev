76bb28254d75cfa28a9a98c3e656cbedae1e96f5
Alessandro Lelli <alessandro.lelli.external@ynap.com>
2023-06-29 23:22:27 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -86,15 +86,15 @@
         <ul v-for="experience in experiences" :key="experience.id" class="grouplist">
           <li class="mb-12">
             <div class="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:drop-shadow-lg"></div>
               <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="July to December 2017">{{ experience.date }}</header>
               <div class="z-10 sm:col-span-6">
                 <h3 class="font-medium leading-snug text-slate-200">
                   <div>
-                    <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-purple-300 focus-visible:text-purple-300  group/link text-base" :href="experience.url" target="_blank" rel="noreferrer" aria-label="UI Engineer Co-op at Apple">
-                      <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                    <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300  group/link text-base" :href="experience.url" target="_blank" rel="noreferrer" aria-label="UI Engineer Co-op at Apple">
+                      <span class="absolute -inset-x-4 -inset-y-2.5 hidden md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                       <span>
-                        {{ experience.jobTitle }}
+                        {{ experience.jobTitle }} - 
                         <span class="inline-block">{{ experience.company }}
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
                             <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
@@ -107,7 +107,7 @@
                 <p class="mt-2 text-sm leading-normal">{{ experience.description }}</p>
                 <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
                   <li v-for="technology in experience.technologies" :key="technology.id" class="mr-1.5 mt-2">
-                    <div class="flex items-center rounded-full bg-purple-400/10 px-3 py-1 text-xs font-medium leading-5 text-purple-300">{{ technology.name }}</div>
+                    <div class="flex items-center bg-sky-400/10 px-3 py-1 text-xs font-medium leading-5 text-sky-300">{{ technology.name }}</div>
                   </li>
                 </ul>
               </div>
@@ -117,9 +117,9 @@
         <div class="mt-12">
           <a class="inline-flex items-center font-medium leading-tight text-slate-200 font-semibold text-slate-200 group" aria-label="View Full Résumé" href="/resume.pdf">
             <span>
-              <span class="border-b border-transparent pb-px transition group-hover:border-purple-300 motion-reduce:transition-none">View Full </span>
+              <span class="border-b border-transparent pb-px transition group-hover:border-sky-300 motion-reduce:transition-none">View Full </span>
               <span class="whitespace-nowrap">
-                <span class="border-b border-transparent pb-px transition group-hover:border-purple-300 motion-reduce:transition-none">Résumé</span>
+                <span class="border-b border-transparent pb-px transition group-hover:border-sky-300 motion-reduce:transition-none">Résumé</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2 motion-reduce:transition-none" aria-hidden="true">
                   <path fill-rule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clip-rule="evenodd"></path>
                 </svg>
@@ -136,11 +136,11 @@
         <ul v-for="project in projects" :key="project.id" class="grouplist">
           <li class="mb-12">
             <div class="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-              <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+              <div class="absolute -inset-x-4 -inset-y-4 z-0 hidden transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:drop-shadow-lg"></div>
               <div class="z-10 sm:order-2 sm:col-span-6">
                 <h3>
-                  <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-purple-300 focus-visible:text-purple-300  group/link text-base" :href="project.url" target="_blank" rel="noreferrer" aria-label="Build a Spotify Connected App">
-                    <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                  <a class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-sky-300 focus-visible:text-sky-300  group/link text-base" :href="project.url" target="_blank" rel="noreferrer" aria-label="Build a Spotify Connected App">
+                    <span class="absolute -inset-x-4 -inset-y-2.5 hidden md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                     <span class="inline-block">{{ project.title }}
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
@@ -150,7 +150,7 @@
                 </h3>
                 <p class="mt-2 text-sm leading-normal">{{ project.description }}</p>
               </div>
-              <img alt="" loading="lazy" width="200" height="48" decoding="async" data-nimg="1" class="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1 aspect-[98/61] object-cover" :src="`images/${project.imageName}`" style="color: transparent;">
+              <img alt="" loading="lazy" width="200" height="48" decoding="async" data-nimg="1" class="border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1 aspect-[98/61] object-cover" :src="`images/${project.imageName}`" style="color: transparent;">
             </div>
           </li>
         </ul>
@@ -159,15 +159,15 @@
       <footer>
         <p class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
           Design Inspired by
-          <a href="https://brittanychiang.com/" class="font-medium text-slate-400 hover:text-purple-300 focus-visible:text-purple-300" target="_blank" rel="noreferrer">Brittany Chiang</a>
+          <a href="https://brittanychiang.com/" class="font-medium text-slate-400 hover:text-sky-300 focus-visible:text-sky-300" target="_blank" rel="noreferrer">Brittany Chiang</a>
           website, but built with
-          <a href="https://nuxt.com/" class="font-medium text-slate-400 hover:text-purple-300 focus-visible:text-purple-300" target="_blank" rel="noreferrer">Nuxt</a>
+          <a href="https://nuxt.com/" class="font-medium text-slate-400 hover:text-sky-300 focus-visible:text-sky-300" target="_blank" rel="noreferrer">Nuxt</a>
           from scratch using
-          <a href="https://tailwindcss.com/" class="font-medium text-slate-400 hover:text-purple-300 focus-visible:text-purple-300" target="_blank" rel="noreferrer">Tailwind CSS</a>
+          <a href="https://tailwindcss.com/" class="font-medium text-slate-400 hover:text-sky-300 focus-visible:text-sky-300" target="_blank" rel="noreferrer">Tailwind CSS</a>
           and deployed with
-          <a href="https://www.netlify.com/" class="font-medium text-slate-400 hover:text-purple-300 focus-visible:text-purple-300" target="_blank" rel="noreferrer">Netlify</a>.
+          <a href="https://www.netlify.com/" class="font-medium text-slate-400 hover:text-sky-300 focus-visible:text-sky-300" target="_blank" rel="noreferrer">Netlify</a>.
           All text is set in the
-          <a href="https://rsms.me/inter/" class="font-medium text-slate-400 hover:text-purple-300 focus-visible:text-purple-300" target="_blank" rel="noreferrer">Inter</a>
+          <a href="https://rsms.me/inter/" class="font-medium text-slate-400 hover:text-sky-300 focus-visible:text-sky-300" target="_blank" rel="noreferrer">Inter</a>
           typeface.
         </p>
       </footer>
@@ -219,6 +219,7 @@ export default {
           id: 2,
           date: "April 2018 - March 2019",
           url: "https://www.cartsan.it/",
+          company: "Cartsan Srl",
           jobTitle: "IT Customer Support",
           description: "In addition to my primary responsibility of providing customer support and troubleshooting medical software, I was fortunate to have my boss recognize my passion for development. This led to an exciting opportunity where I was entrusted with developing an internal management system for the company. Through this experience, I acquired proficiency in PHP programming, utilizing MySQL as a database, and further refining my knowledge of HTML and CSS.",
           technologies: [
@@ -240,6 +241,7 @@ export default {
           id: 3,
           date: "June 2015 - July 2015",
           url: "http://www.videoarts.eu/",
+          company: "Videoarts Webdesign",
           jobTitle: "IT Intern",
           description: "As an intern at Fabio Mosti's Videoarts Webdesign, a small yet impactful company, I had the opportunity to immerse myself in the professional world. Under Fabio's guidance, I delved into the realm of object-oriented programming and embarked on a practical journey. During my tenure, I successfully developed a Java-based management software specifically tailored for a kennel. This invaluable experience further expanded my practical skills and solidified my understanding of software development.",
           technologies: [
